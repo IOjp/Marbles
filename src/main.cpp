7ff81f580f13011589bf50f30aaa1583b99968dd
Jesse Paul <iojp.soft@yahoo.com>
2014-09-29 08:14:18 -0400

    Changes to Hash and MerkleRoot

--- src/main.cpp
+++ src/main.cpp
@@ -2554,8 +2554,8 @@ bool LoadBlockIndex(bool fAllowNew)
         block.nVersion = 1;
         block.nTime    = 1411944258;
         block.nBits    = bnProofOfWorkLimit.GetCompact();
-        block.nNonce   = 193526;
-        if (true ) {
+        block.nNonce   = 451048;
+        if (false ) {
 
         // This will figure out a valid hash and Nonce if you're
         // creating a different genesis block:
@@ -2577,7 +2577,7 @@ bool LoadBlockIndex(bool fAllowNew)
         printf("block.nTime = %u \n", block.nTime);
         printf("block.nNonce = %u \n", block.nNonce);
 
-        assert(block.hashMerkleRoot == uint256("cd8287c2504056140115cca261f9df5a6c8e6952dc485f909050b6120f89c60b"));
+        assert(block.hashMerkleRoot == uint256("000002d4597fcc7ec9239142cf4674f27d5e81ecc0fb7c1962ae05cc905a5942"));
 		    assert(block.GetHash() == (!fTestNet ? hashGenesisBlock : hashGenesisBlockTestNet));
 
         // Start new block file
